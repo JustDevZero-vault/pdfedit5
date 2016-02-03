df1d1114a78c177fe4336bcc3bf1d333793d51fa
AliYousuf <aliyousuf2012@hotmail.com>
2016-02-03 12:13:29 +0400

    Move headers from Xpdf to Poppler on tests/Kernel

--- src/tests/kernel/testcpage.cc
+++ src/tests/kernel/testcpage.cc
@@ -24,7 +24,7 @@
 // vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
 
 #include "kernel/static.h"
-#include "xpdf/PDFDoc.h"
+#include "puppler/poppler/PDFDoc.h"
 #include "tests/kernel/testmain.h"
 #include "tests/kernel/testcobject.h"
 #include "tests/kernel/testcpage.h"
